c579c9966d5e9711fefeea181fb28af8fd5a4304
F1uffyDe5troyer <Dalarnoricardomilago@gmail.com>
2022-01-21 11:58:43 +0200

    finished 19

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header="header" :text="text" theme="promotion"/>
+  <p>Have a look at our promotion</p>
+  <teleport to=".modals" v-if="showModal">
+  <div v-if="showModal">
+    <Modal theme="promotion" @close="toggleModal" >
+      <template v-slot:links>
+        <a href="#">Register</a>
+        <a href="#">Find Out More</a>
+      </template>
+      <h1>Young Grasshopper</h1>
+      <p>Come get your 50% discount now whilst stock last!</p>
+    </Modal>
+  </div>
+  </teleport>
+  <!-- <button @click="toggleModal">Open Modal</button> -->
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo" >
+      <h1>Register for promotions</h1>
+      <p>Do not miss out of upcoming promo codes!</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Info</button>
+  &nbsp;
+  <button @click="toggleModalTwo">Register</button>
 </template>
 
 <script>
@@ -11,18 +34,25 @@ export default {
     Modal
   },
   data() {
-   return {
-     title: 'Young Grasshopper',
-     header: 'Sign up to win a prize',
-     text: 'Come get your 50% discount now whilst stocks last!',
-   }
+    return {
+      title: 'Young Grasshopper',
+      header: 'Sign up to win a prize!',
+      text: 'Come get your 50% discount now whilst stock last!',
+      showModal: false,
+      showModal: false,
+      showModalTwo: false
+    }
   },
-methods: {
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
   }
 }
-
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -31,5 +61,10 @@ methods: {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,43 +1,76 @@
 <template>
-  <div class="backdrop">
-      <div class="modal" :class="{promotion: theme === 'promotion'}">
-          <h1>{{ header }}</h1>
-          <p>{{ text }}</p>
-      </div>
-  </div>
+    <!-- Only when we click on this div will the modal close (self) -->
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{promotion: theme === 'promotion'}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
 </template>
+
+
 
 <script>
 export default {
-  props:['header', 'text', 'theme']
+    props:['header', 'text', 'theme'],
+    methods: {
+      closeModal() {
+        this.$emit('close');
+      }
+    }
 }
 </script>
 
+
+
 <style>
 .modal {
-  width: 400px;
-  padding: 20px;
-  margin: 100px auto;
-  background: white;
-  border-radius: 10px;
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
 }
 .backdrop {
-  top: 0;
-  position: fixed;
-  background: rgba(0, 0, 0, 0.5);
-  width: 100%;
-  height: 100%;
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
 }
 .modal h1 {
     color: #03cfb4;
     border: none;
     padding: 0;
 }
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+.modal .actions a {
+    color:#333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
 .modal.promotion {
     background: crimson;
     color: white;
 }
-.modal.promotion {
+.modal.promotion h1 {
+    color: white;
+}
+.modal.promotion .actions {
+    color: white;
+}
+.modal.promotion .actions a {
     color: white;
 }
 </style>
+
